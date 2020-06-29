b687ed998f587f2df233f66ffe34c82c813c6fd8
Matteo Andruccioli <matteo.andruccioli2@studio.unibo.it>
2020-06-29 15:38:58 +0200

    fixed navbar css

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -1,39 +1,39 @@
 <template>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar1" aria-controls="navbar1"
-            aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container row d-flex mx-auto" style="width: 80%">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar1" aria-controls="navbar1"
+        aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
 
-        <div class="collapse navbar-collapse justify-content-md-center" id="navbar1">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <router-link class="nav-link" to="/">Home</router-link>
-                </li>
-                <li v-if="!isUserLoggedIn" class="nav-item">
-                    <router-link class="nav-link" to="/login">Login</router-link>
-                </li>
-                <li v-if="!isUserLoggedIn" class="nav-item">
-                    <router-link class="nav-link" to="/register">Register</router-link>
-                </li>
-                <li v-if="isCustomer" class="nav-item">
-                    <router-link class="nav-link" to="/customerProfile">Customer Profile</router-link>
-                </li>
-                <li v-if="isMasseur" class="nav-item">
-                    <router-link class="nav-link" :to="'/masseurProfile/' + this.currentUserId">Masseur Profile</router-link>
-                </li>
-                <li v-if="isUserLoggedIn" class="nav-item">
-                    <a class="nav-link" href="/" v-on:click="logout">Logout</a>
-                </li>
-                <li v-if="isUserLoggedIn" class="nav-item">  <!-- TEST ONLY -->
-                    <a class="nav-link" v-on:click="getUserInfo">GetUserInfo</a>
-                </li>
-                <li>
-                  <NewsMainButton v-if="showNewsMainButton"/>
-                </li>
-            </ul>
-        </div>
-    </nav>
+      <div class="collapse navbar-collapse" id="navbar1">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+              <router-link class="nav-link" to="/">Home</router-link>
+          </li>
+          <li v-if="!isUserLoggedIn" class="nav-item">
+              <router-link class="nav-link" to="/login">Login</router-link>
+          </li>
+          <li v-if="!isUserLoggedIn" class="nav-item">
+              <router-link class="nav-link" to="/register">Register</router-link>
+          </li>
+          <li v-if="isCustomer" class="nav-item">
+              <router-link class="nav-link" to="/customerProfile">Customer Profile</router-link>
+          </li>
+          <li v-if="isMasseur" class="nav-item">
+              <router-link class="nav-link" :to="'/masseurProfile/' + this.currentUserId">Masseur Profile</router-link>
+          </li>
+          <li v-if="isUserLoggedIn" class="nav-item">
+              <a class="nav-link" href="/" v-on:click="logout">Logout</a>
+          </li>
+          <li v-if="isUserLoggedIn" class="nav-item">  <!-- TEST ONLY -->
+              <a class="nav-link" v-on:click="getUserInfo">GetUserInfo</a>
+          </li>
+        </ul>
+      </div>
+      <NewsMainButton v-if="showNewsMainButton"/>
+    </div>
+  </nav>
 </template>
 
 <script>
