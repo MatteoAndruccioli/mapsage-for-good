978eed4466bf66ba9010c03c4458b5ca55b25a96
Matteo Andruccioli <matteo.andruccioli2@studio.unibo.it>
2020-07-05 12:11:41 +0200

    refactor of routes/*

--- backend/routes/usersRoutes.js
+++ backend/routes/usersRoutes.js
@@ -1,9 +1,8 @@
+//these routes manage users log-in and log-out
 module.exports = function(app) {
 	var usersController = require('../controllers/usersController')
 
-  app.route('/users/login')
-    .post(usersController.handleLoginRequest)
+  app.route('/users/login').post(usersController.handleLoginRequest)
 
-	app.route('/users/logout')
-		.get(usersController.handleLogoutRequest)
+	app.route('/users/logout').get(usersController.handleLogoutRequest)
 };
